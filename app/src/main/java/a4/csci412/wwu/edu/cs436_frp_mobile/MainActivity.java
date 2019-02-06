76ab31365354fabbc9199ea9ea8b073d0d99ced9
foster22 <foster22@wwu.edu>
2019-02-06 10:20:58 -0800

    initial commit

---
+++ app/src/main/java/a4/csci412/wwu/edu/cs436_frp_mobile/MainActivity.java
@@ -0,0 +1,13 @@
+package a4.csci412.wwu.edu.cs436_frp_mobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
