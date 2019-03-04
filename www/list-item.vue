01c4075d2fb13a1ebfe6356cfea4147f63b9689f
mcfoster102 <33943541+mcfoster102@users.noreply.github.com>
2019-03-04 10:30:57 -0800

    added a list item vue file

---
+++ www/list-item.vue
@@ -0,0 +1,45 @@
+<template>
+    <Page class="page">
+        <ActionBar title="Home" class="action-bar" />
+        <template>
+
+            <StackLayout orientation="horizontal" width="400" height="100"
+                backgroundColor="lightblue">
+                <TextField v-model="itemValue" hint="Item..." width="150" />
+                <TextField v-model="itemValue" hint="Amount..." width="75" />
+                <!--<Text {{amountCount}} width="75" />-->
+                <ListPicker :items="listPickerUnits" v-model="selectedListPickerIndex"
+                    width="50" />
+                <!--<button v-bind:onPress="changeCount" :title="plus"/>-->
+                <Button text="+" @tap="onButtonTap" width="40" height="40" />
+                <Button text="-" @tap="onButtonTap" width="40" height="40" />
+            </StackLayout>
+
+        </template>
+    </Page>
+</template>
+<script>
+    export default {
+        methods: {
+            //changeCount(){
+            //    this.amountCount = this.amountCount
+            //}
+            onButtonTap() {
+                console.log("Button was pressed");
+            }
+        },
+
+        data() {
+            return {
+                listPickerUnits: ["lbs", "cases", "boxes"],
+                selectedListPickerIndex: 0,
+
+                itemValue: "",
+                amountCount: 0
+            };
+        }
+    };
+</script>
+
+<style>
+</style>
