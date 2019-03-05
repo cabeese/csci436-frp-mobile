8f02a54cb2eabfc91aec5b25d87cfba98a270764
mcfoster102 <33943541+mcfoster102@users.noreply.github.com>
2019-03-05 10:32:23 -0800

    fixed list-item

--- www/list-item.vue
+++ www/list-item.vue
@@ -5,14 +5,15 @@
 
             <StackLayout orientation="horizontal" width="400" height="100"
                 backgroundColor="lightblue">
-                <TextField v-model="itemValue" hint="Item..." width="150" />
-                <TextField v-model="itemValue" hint="Amount..." width="75" />
-                <!--<Text {{amountCount}} width="75" />-->
+                <TextField v-model="itemValue" hint="Item..." width="150" height="50"/>
+                <!--<TextField v-model="itemValue" hint="Amount..." width="75" />-->
+                <TextView keyboardType="number" v-model="amountCount" hint="Amount..." width="50" height="50"/>
                 <ListPicker :items="listPickerUnits" v-model="selectedListPickerIndex"
-                    width="50" />
-                <!--<button v-bind:onPress="changeCount" :title="plus"/>-->
-                <Button text="+" @tap="onButtonTap" width="40" height="40" />
-                <Button text="-" @tap="onButtonTap" width="40" height="40" />
+                    width="75" />
+                <Button text="+" @tap="changeCountPlus" width="40" height="40" />
+                <Button text="-" @tap="changeCountMinus" width="40" height="40" />
+                <!--<Button text="+" @tap="onButtonTap" width="40" height="40" />-->
+                <!--<Button text="-" @tap="onButtonTap" width="40" height="40" />-->
             </StackLayout>
 
         </template>
@@ -21,19 +22,20 @@
 <script>
     export default {
         methods: {
-            //changeCount(){
-            //    this.amountCount = this.amountCount
-            //}
-            onButtonTap() {
-                console.log("Button was pressed");
+            changeCountPlus() {
+                this.amountCount = this.amountCount + 1;
+            },
+            changeCountMinus() {
+                this.amountCount = this.amountCount - 1;
             }
+            /*onButtonTap() {
+                console.log("Button was pressed");
+            }*/
         },
-
         data() {
             return {
                 listPickerUnits: ["lbs", "cases", "boxes"],
                 selectedListPickerIndex: 0,
-
                 itemValue: "",
                 amountCount: 0
             };
