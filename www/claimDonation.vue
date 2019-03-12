5239578ef053ed6a5980396fcb04e4fa1c127303
hartnea <hartnea@wwu.edu>
2019-03-12 15:43:48 -0700

    Created claim donation

---
+++ www/claimDonation.vue
@@ -0,0 +1,79 @@
+<template>
+    <Page class="page">
+        <ActionBar title="Claim a Donation" class="action-bar" />
+        <template>
+            <StackLayout orientation="vertical">
+                <Label text="Click on an item to get more information" />
+                <ListView for="(item, index) in donations" height="700" separatorColor="transparent">
+                    <v-template>
+                        <TextView editable="false" @tap="moreInfo(index)" id="textView">
+                            {{item.contactName}}
+                            {{item.location}}
+                        </TextView>
+                    </v-template>
+                </ListView>
+            </StackLayout>
+        </template>
+    </Page>
+</template>
+
+<script>
+    import showItem from "./showItem";
+    export default {
+        methods: {
+            moreInfo(index) {
+                console.log("Item with index: " + index + " tapped");
+                this.$navigateTo(showItem, {props: {donation: this.donations[index] }});
+            }
+        },
+
+        data() {
+            return {
+                donations: [{
+                        contactName: "Ashlyn H",
+                        contactPhone: "3609011803",
+                        location: "Bellingham",
+                        items: [{
+                                itemName: "Blueberries",
+                                itemAmount: "2",
+                                type: "lbs"
+                            },
+                            {
+                                itemName: "Granola",
+                                itemAmount: "5",
+                                type: "boxes"
+                            }
+                        ]
+                    },
+                    {
+                        contactName: "Ashlyn H",
+                        contactPhone: "3609011803",
+                        location: "Bellingham",
+                        items: [{
+                                itemName: "Blueberries",
+                                itemAmount: "2",
+                                type: "lbs"
+                            },
+                            {
+                                itemName: "Granola",
+                                itemAmount: "5",
+                                type: "boxes"
+                            }
+                        ]
+                    }
+                ]
+            };
+        }
+    };
+</script>
+
+<style>
+    Label {
+        color: black;
+        font-size: 16px;
+        margin-bottom: 5px;
+        margin-top: 10px;
+    }
+
+
+</style>
---
+++ www/showItem.vue
@@ -0,0 +1,40 @@
+<template>
+    <Page class="page">
+        <ActionBar title="Claim a Donation" class="action-bar" />
+        <template>
+            <StackLayout orientation="vertical">
+                    <StackLayout class="home-panel">
+                        <Label class="h2 description-label" :text="$props.donation.contactName" />
+                        <Label class="h2 description-label" :text="$props.donation.contactPhone" />
+                        <Label class="h2 description-label" :text="$props.donation.location" />
+                        <ListView for="(item, index) in $props.donation.items"
+                            height="700" separatorColor="transparent">
+                            <v-template>
+                                <TextView editable="false" id="textView">
+                                    {{item.itemName}} {{item.itemAmount}}
+                                    {{item.type}}
+                                </TextView>
+                            </v-template>
+                        </ListView>
+                        <Button text="Claim Donation" @tap="" />
+                    </StackLayout>
+
+            
+
+            </StackLayout>
+
+        </template>
+    </Page>
+</template>
+
+<script>
+    export default {
+        props: ["donation"],
+        data() {
+            return {};
+        }
+    };
+</script>
+
+<style>
+</style>
