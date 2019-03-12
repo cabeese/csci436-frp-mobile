4f74bba1b88c846e00caade9866903928407e04e
hartnea <hartnea@wwu.edu>
2019-03-12 16:18:57 -0700

    Updated look of Claim page

--- www/showItem.vue
+++ www/showItem.vue
@@ -3,23 +3,27 @@
         <ActionBar title="Claim a Donation" class="action-bar" />
         <template>
             <StackLayout orientation="vertical">
-                    <StackLayout class="home-panel">
-                        <Label class="h2 description-label" :text="$props.donation.contactName" />
-                        <Label class="h2 description-label" :text="$props.donation.contactPhone" />
-                        <Label class="h2 description-label" :text="$props.donation.location" />
-                        <ListView for="(item, index) in $props.donation.items"
-                            height="700" separatorColor="transparent">
-                            <v-template>
-                                <TextView editable="false" id="textView">
-                                    {{item.itemName}} {{item.itemAmount}}
-                                    {{item.type}}
-                                </TextView>
-                            </v-template>
-                        </ListView>
-                        <Button text="Claim Donation" @tap="" />
-                    </StackLayout>
+                <Label class="h2 description-label" text="Contact Information" />
+                <FlexboxLayout flexDirection="column" class="text-center">
+                    <Label class="h3 description-label" :text="$props.donation.contactName" />
+                    <Label class="h3 description-label" :text="$props.donation.contactPhone" />
+                    <Label class="h3 description-label" :text="$props.donation.location" />
+                </FlexboxLayout>
+                <Label class="h2 description-label" text="Food to Claim" />
+                <ScrollView>
+                    <ListView for="(item, index) in $props.donation.items"
+                        height="375" separatorColor="transparent">
+                        <v-template>
+                            <TextView editable="false" id="textView">{{item.itemName}} {{item.itemAmount}} {{item.type}}
+                            </TextView>
+                        </v-template>
 
-            
+                    </ListView>
+                </ScrollView>
+
+
+                <Button text="Claim Donation" @tap="" width="100" />
+
 
             </StackLayout>
 
@@ -37,4 +41,7 @@
 </script>
 
 <style>
+    Button {
+        font-size: 15px;
+    }
 </style>
