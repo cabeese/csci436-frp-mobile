cfc68be9e0ac5389e2a8fe0f61d648b25c3cfdc1
Noah <strongn@wwu.edu>
2019-03-17 19:42:43 -0700

    Adds action (toggleFoo) to example component

--- src/components/FooViewer.js
+++ src/components/FooViewer.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
 import { connect } from 'react-redux'
+import { toggleFoo } from '../redux/actions'
 
 class FooViewer extends React.Component {
   render() {
     return (
       <View>
-          <Text>This is the Foo Viewer. Foo is {this.props.foo ? "true" : "false"}</Text>
+        <Button title="Toggle Foo" onPress={this.props.toggleFoo} />
+        <Text>This is the Foo Viewer. Foo is {this.props.foo ? "true" : "false"}</Text>
       </View>
     );
   }
@@ -17,6 +19,11 @@ const mapStateToProps = state => {
   return { foo };
 };
 
+const actionCreators = {
+  toggleFoo
+};
+
 export default connect(
   mapStateToProps,
+  actionCreators
 )(FooViewer);
