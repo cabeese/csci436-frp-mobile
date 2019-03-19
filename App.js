90ca4bb9a7f8ca563fc8baecb0484e93bf522706
foster22 <foster22@wwu.edu>
2019-03-19 16:07:12 -0700

    basic list-item complete

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Button } from 'react-native';
 import ListItem from './source/components/list-item'
 
 export default class App extends React.Component {
---
+++ www/components/line-item.js
@@ -0,0 +1,15 @@
+//https://github.com/ankitsinghania94/vue-native-todo-app/blob/master/src/TodoItem.vue
+//https://github.com/ankitsinghania94/vue-native-todo-app/blob/master/src/AddTodo.vue
+Vue.component('line-item', {
+  props: {
+    name: String,
+    number: Number,
+    unit: String,
+
+  }
+  data: function(){
+    return {
+
+    }
+  }
+})
