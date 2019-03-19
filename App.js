23ac8a8b30002fe0077767a36b6cc27a94f64663
Noah <strongn@wwu.edu>
2019-03-18 20:09:22 -0700

    Remove FooViewer

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import store from './src/redux/store'
-import FooViewer from './src/components/FooViewer'
 import DonationList from './src/components/donation-list';
 
 export default class App extends React.Component {
@@ -10,10 +9,6 @@ export default class App extends React.Component {
     return (
       <Provider store={store}>
         <View style={styles.container}>
-          <Text>Open up App.js to start working on your app!</Text>
-
-          <FooViewer />
-
           <DonationList />
         </View>
       </Provider>
