ce1f948972aa44c014f249aa013bd7786bdb4ce9
Noah <strongn@wwu.edu>
2019-03-18 20:18:47 -0700

    Semi-resets App.js to original template, but with comments

--- App.js
+++ App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import store from './src/redux/store'
-import DonationList from './src/components/donation-list';
+// Could import donation list:
+//   import DonationList from './src/components/donation-list';
+// and then render it in the body with
+//   <DonationList />
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
         <View style={styles.container}>
-          <DonationList />
+          <Text>Open up App.js to start working on your app!</Text>
         </View>
       </Provider>
     );
