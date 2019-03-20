be7c192489f1d2c6ad5efde98b142db3f2a2df26
Noah <strongn@wwu.edu>
2019-03-20 14:13:20 -0700

    Update store to fetch donation items, include 'activeDonation'

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,4 @@
-import { TOGGLE_FOO, CALL_FETCH_DONATIONS, FETCH_DONATIONS_FAIL, FETCH_DONATIONS_SUCC } from "./actionTypes";
+import { TOGGLE_FOO, CALL_FETCH_DONATIONS, FETCH_DONATIONS_FAIL, FETCH_DONATIONS_SUCC, SET_ACTIVE_DONATION } from "./actionTypes";
 
 const ROOT_URL = "https://foodbanks-staging.herokuapp.com/mob/v1/donation/";
 
@@ -6,11 +6,16 @@ export const toggleFoo = () => ({
   type: TOGGLE_FOO,
 });
 
+export const setActiveDonation = (id) => ({
+  type: SET_ACTIVE_DONATION,
+  id: id || 0,
+});
+
 export const fetchDonations = () => {
   return async (dispatch) => {
     dispatch({ type: CALL_FETCH_DONATIONS });
 
-    const response = await fetch(ROOT_URL + "get-avail");
+    const response = await fetch(ROOT_URL + "get-avail?includeItems=true");
     if(response.ok){
       const donations = await response.json();
       dispatch(fetchDonationsSucc(donations));
